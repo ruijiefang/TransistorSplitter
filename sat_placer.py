0f0200562e02ed3f211f346a8d4f507a14f4de19
Ruijie Fang <ruijief@princeton.edu>
2025-04-03 18:47:34 -0500

    commence work on sat-based placer

---
+++ sat_placer.py
@@ -0,0 +1,12 @@
+
+"""
+  SAT-driven placement
+  https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=10473978 
+"""
+
+class SATPlacement(object):
+
+  def __init__(self, num_rows, num_sites):
+          
+
+
